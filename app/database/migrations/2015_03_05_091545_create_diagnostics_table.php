451ef1ec44b451ad1b820a3431223357026eda84
ravi <ravikukkadapu.2762@gmail.com>
2015-03-05 14:46:58 +0530

    new table created for diagnostics

--- app/database/migrations/2015_03_05_091545_create_diagnostics_table.php
+++ app/database/migrations/2015_03_05_091545_create_diagnostics_table.php
@@ -17,14 +17,14 @@ class CreateDiagnosticsTable extends Migration {
 			$table->increments('id');
 			$table->string('patient_code',30);
 			$table->string('heartbeat_value',10);
-			$table->datetime('heartbeatcheck_date',10);
+			$table->string('heartbeatcheck_date',10);
 			$table->string('bpcystolic_value',10);
 			$table->string('bpdystolic_value',10);
-			$table->datetime('bpcheck_date',10);
+			$table->string('bpcheck_date',10);
 			$table->string('bloodsugar_fasting',10);
 			$table->string('bloodsugar_random',10);
 			$table->string('bloodsugar_postlunch',10);
-			$table->datetime('bloodsugarcheck_date',30);
+			$table->string('bloodsugarcheck_date',30);
 			$table->string('patient_ip',20);
 			$table->timestamps();
 		});
