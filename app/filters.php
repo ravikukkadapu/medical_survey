8b7a36f36c5d24b968fd27f90e4c2bfa6d174d27
ravi <ravikukkadapu.2762@gmail.com>
2015-05-11 18:00:16 +0530

    get request

--- app/filters.php
+++ app/filters.php
@@ -88,3 +88,12 @@ Route::filter('csrf', function()
 		throw new Illuminate\Session\TokenMismatchException;
 	}
 });
+
+
+App::before(function($request)
+{
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+    header('Access-Control-Allow-Credentials: true');
+});
